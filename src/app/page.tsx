c2274ce54c18565c4a6344b34df98d4fb4d697c7
Asiqur Rahman <ashiqurrahman248225@gmail.com>
2026-01-08 06:08:20 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
 import { redirect } from 'next/navigation';
-import DashboardLayout from './dashboard/layout';
-import Dashboard from './dashboard/page';
 
 export default function RootPage() {
-  // By default, show the dashboard.
-  return (
-    <DashboardLayout>
-      <Dashboard />
-    </DashboardLayout>
-  )
+  // By default, redirect to the dashboard.
+  redirect('/dashboard');
 }
