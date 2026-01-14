79e23f9d442f3658afbd9e24b00cead70c51d7dc
Asiqur Rahman <ashiqurrahman248225@gmail.com>
2026-01-14 13:39:48 +0000

    not fit in screen.. and don't response with screen size.. (_for element

--- src/app/performance/mistakes/page.tsx
+++ src/app/performance/mistakes/page.tsx
@@ -140,7 +140,7 @@ export default function MistakenWordsPage() {
     }
 
     return (
-         <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+         <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 overflow-x-auto">
             <div className="flex items-center justify-between">
                 <div>
                     <h1 className="text-2xl font-bold">All Mistaken Words</h1>
